39a966015b207901b9b3b70a4be9c889d7811dca
TEDRudraksh <tedrudrakshtanwar@gmail.com>
2026-09-10 22:54:10 +0530

    add palindrome program

---
+++ logic build 1/palindrome.py
@@ -0,0 +1,14 @@
+def palin():
+    num = int(input("enter the number"))
+
+    original = num
+    reverse = 0
+    while num> 0:
+        digit = num % 10
+        reverse = reverse * 10 + digit
+        num = num // 10
+    if original == reverse :
+        print("it is a palindrome")
+    else:
+        print("it is not a palindrome")
+palin()
